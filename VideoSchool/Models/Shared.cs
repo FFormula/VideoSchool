71e513c913c0c30d734b342c77902d2a8f2f250a
Jevgenij Volosatov <fformula@gmail.com>
2016-03-08 16:07:00 +0200

    Share sql and error

---
+++ VideoSchool/Models/Shared.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoSchool.Models
+{
+    public class Shared
+    {
+        public MySQL sql { get; private set; }
+        public Error error { get; private set; }
+        public Controller controller { get; private set; }
+
+        public Shared (Controller controller)
+        {
+            this.controller = controller;
+            error = new Error();
+            sql = new MySQL(error);
+        }
+
+    }
+}
