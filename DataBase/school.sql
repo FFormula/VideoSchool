35661435a4de78dcbf933b94039b705b5b9a3e57
Jevgenij Volosatov <fformula@gmail.com>
2016-03-08 16:53:40 +0200

    MySQL dump

---
+++ DataBase/school.sql
@@ -0,0 +1,157 @@
+-- MySQL dump 10.13  Distrib 5.6.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: SCHOOL
+-- ------------------------------------------------------
+-- Server version	5.6.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action`
+--
+
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№ действия',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'Код действия для проверки',
+  `info` text NOT NULL COMMENT 'Что включает в себя это действие',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0-отключено, 1-работает',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Индекс 2` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='Список всех возможных действий в системе';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `action`
+--
+
+LOCK TABLES `action` WRITE;
+/*!40000 ALTER TABLE `action` DISABLE KEYS */;
+INSERT INTO `action` VALUES (1,'user_Update','-',1),(2,'role_Update','-',1),(3,'role_EditUser','-',1),(4,'role_EditAction','-',1),(5,'action_Update','-',1);
+/*!40000 ALTER TABLE `action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№ роли',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'Название роли',
+  `info` text COMMENT 'Описание ролевой сути',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список ролевых статусов проекта';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_action`
+--
+
+DROP TABLE IF EXISTS `role_action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_action` (
+  `role_id` int(11) NOT NULL COMMENT '№ роли',
+  `action_id` int(11) NOT NULL COMMENT '№ действия',
+  PRIMARY KEY (`role_id`,`action_id`),
+  KEY `FK_role_action_action` (`action_id`),
+  CONSTRAINT `FK_role_action_action` FOREIGN KEY (`action_id`) REFERENCES `action` (`id`),
+  CONSTRAINT `FK_role_action_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список доступных действий для каждой роли';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_action`
+--
+
+LOCK TABLES `role_action` WRITE;
+/*!40000 ALTER TABLE `role_action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_user`
+--
+
+DROP TABLE IF EXISTS `role_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_user` (
+  `role_id` int(11) NOT NULL COMMENT '№ роли',
+  `user_id` int(11) NOT NULL COMMENT '№ пользователя',
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `FK_role_user_user` (`user_id`),
+  CONSTRAINT `FK__role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FK_role_user_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Какой пользователь какой ролью обладает';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_user`
+--
+
+LOCK TABLES `role_user` WRITE;
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№ пользователя',
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL COMMENT 'Электропочта для авторизации',
+  `passw` varchar(255) DEFAULT NULL COMMENT 'Закодированный пароль',
+  `status` int(11) DEFAULT '0' COMMENT '0-нет доступа, 1-есть',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='Список всех пользователей системы';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Jevgenij','fformula@gmail.com','*E75A663C6384E4833CBD81F37797B862017C2555',1),(2,'Valera','walera@yandex.ru','*CEFAE98A0AE1BD158A659D26CEF833E5035AD7BC',1),(3,'Muza','hely@muza.org','*E75A663C6384E4833CBD81F37797B862017C2555',1),(4,'111','walera@yandex.ru1','*E75A663C6384E4833CBD81F37797B862017C2555',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-08 16:51:46
