740a447eea26c008bcf13c73d90b927db6402e23
Jevgenij Volosatov <fformula@gmail.com>
2016-03-09 12:55:08 +0200

    Add Unit Test project

---
+++ SchoolTest/UserTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoSchool.Models;
+
+namespace SchoolTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        Shared shared;
+
+        public UserTest ()
+        {
+            shared = new Shared();
+        }
+
+        [TestMethod]
+        public void TestSelect()
+        {
+            User user = new User(shared);
+            user.Select("1");
+            Assert.AreEqual("fformula@gmail.com", user.email);
+        }
+    }
+}
