f9d5a092113a9458f02321b0655cc9ffb3ec46ee
Mikhail158 <ermichin1999@gmail.com>
2016-03-10 21:20:16 +0300

    Added basic unit tests

--- VideoSchool/SchoolTest/UserTest.cs
+++ VideoSchool/SchoolTest/UserTest.cs
@@ -7,14 +7,71 @@ namespace SchoolTest
     [TestClass]
     public class UserTest
     {
+        Shared shared;
+
         [TestMethod]
         public void TestSelect()
         {
-            Shared shared = new Shared(RunMode.UnitTest);
+            shared = new Shared(RunMode.UnitTest);
+
             //shared.config.SaveFromTest();
             User user = new User(shared);
             user.Select("1");
             Assert.AreEqual("fformula@gmail.com", user.email);
+        }
+
+        [TestMethod]
+        public void TestInsertAndDelete()
+        {
+            shared = new Shared(RunMode.UnitTest);
+
+            var query = @"
+		    INSERT INTO user
+		        SET name = 'TEST_USER',
+		            email = 'test_user@test_email.com',
+		            passw = password('-----'),
+		            status = '1'";
+            var id = shared.db.Insert(query);
+
+            Assert.AreEqual(true, id > 0);
+
+            query = @"
+		    DELETE FROM user
+		        WHERE name = 'TEST_USER'
+		            AND email = 'test_user@test_email.com'";
+
+            shared.db.Update(query);
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+            shared = new Shared(RunMode.UnitTest);
+
+            var query = @"
+		    INSERT INTO user
+		        SET name = 'TEST_USER',
+		            email = 'test_user@test_email.com',
+		            passw = password('-----'),
+		            status = '1'";
+
+            var id = shared.db.Insert(query);
+            Assert.AreEqual(true, id > 0);
+
+            query = @"
+		    UPDATE user
+                SET name = 'TEST_USER_RENAMED'
+		        WHERE name = 'TEST_USER'
+		            AND email = 'test_user@test_email.com'";
+
+            shared.db.Update(query);
+
+            query = @"
+		    DELETE FROM user
+		        WHERE name = 'TEST_USER_RENAMED'
+		            AND email = 'test_user@test_email.com'";
+
+            shared.db.Update(query);
         }
     }
 }
