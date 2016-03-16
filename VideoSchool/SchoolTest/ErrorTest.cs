06f69dc09fbd5d5bf81a5467315b3f28b4436067
igorenzia <igor941@mail.ru>
2016-03-16 09:57:40 +0300

    тест модели Error

---
+++ VideoSchool/SchoolTest/ErrorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoSchool.Models;
+
+namespace SchoolTest
+{
+    [TestClass]
+    public class ErrorTest
+    {
+        [TestMethod]
+        public void TestMethodClear()
+        {
+            Shared shared =  new Shared(RunMode.UnitTest);
+            Error er = new Error(shared);
+            er.exception = new Exception("Ой ошибочка");
+            er.Clear();
+            Assert.AreEqual(er.mode, ErrorMode.NoErrors);
+            Assert.AreEqual(er.text, String.Empty);
+            Assert.AreEqual(er.exception, null);
+        }
+    }
+}
